eb6f92179196cb4edd7fd13299c65f4cb385b4a1
Benjamin Harris <ben@Benjamins-MacBook-Air-2.local>
2023-04-14 01:22:19 -0400

    updated

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,23 +1,29 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { OpenAIEmbeddings } from 'langchain/embeddings';
-import { PineconeStore } from 'langchain/vectorstores';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { Document } from "langchain/document";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+
+
+
+
+
 
 /* Name of directory to retrieve your files from */
-const filePath = 'docs';
+const filePath = 'docs/peak/peaktest.pdf';
 
 export const run = async () => {
   try {
+    const loader = new TextLoader("docs/peak/sample.txt");
     /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new CustomPDFLoader(path),
-    });
 
     // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+    const rawDocs = await loader.load();
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
@@ -25,7 +31,10 @@ export const run = async () => {
       chunkOverlap: 200,
     });
 
+
+
     const docs = await textSplitter.splitDocuments(rawDocs);
+
     console.log('split docs', docs);
 
     console.log('creating vector store...');
@@ -33,16 +42,20 @@ export const run = async () => {
     const embeddings = new OpenAIEmbeddings();
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
+    
+
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
     });
+    console.log(embeddings)
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
   }
+  
 };
 
 (async () => {
